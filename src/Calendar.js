842346911e27ec549172e9d6522cd2be8264d2fb
polina.lappo <miss.lappo@yandex.ru>
2020-04-19 01:41:11 +0300

    use local storage

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,7 +3,6 @@ import Task from './Task';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import moment from 'moment';
-// import StatusMenu from './StatusMenu'
 
 function getWeekRange(date) {
     return {
@@ -61,33 +60,42 @@ class Calendar extends Component {
 
     addNewTask = event => {
         console.log("add new task");
+        localStorage.plannerApp = JSON.stringify([...this.state.taskData,
+            {
+                "id": JSON.parse(localStorage.plannerApp).length+1,
+                "mo": null,
+                "tu": null,
+                "we": null,
+                "th": null,
+                "fr": null,
+                "sa": null,
+                "su": null,
+                "taskText": ""
+            }])
         this.setState({
-            taskData: [...this.state.taskData, 
-                {
-                    "mo": null,
-                    "tu": null,
-                    "we": null,
-                    "th": null,
-                    "fr": null,
-                    "sa": null,
-                    "su": null,
-                    "taskText": ""
-                }]
+            taskData: JSON.parse(localStorage.plannerApp)
         })
     }
 
     componentDidMount() {
         this.handleDayChange(new Date());
+        this.setState({
+            taskData: JSON.parse(localStorage.plannerApp)
+        })
+        
         // TODO get real fetch request
-        const url = `http://d74ev.mocklab.io/tasks`
-        fetch(url)
-            .then(response => response.json())
-            .then(json => this.setState({
-                taskData: json
-            }))
+        // const url = `http://d74ev.mocklab.io/tasks`
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(json => this.setState({
+        //         taskData: json
+        //     }))
     }
 
     render() {
+        if (!localStorage.plannerApp) {
+            localStorage.plannerApp="[]";
+        }
         const { hoverRange, selectedDays, taskData} = this.state;
 
         const daysAreSelected = selectedDays.length > 0;
@@ -104,7 +112,7 @@ class Calendar extends Component {
             selectedRangeEnd: daysAreSelected && selectedDays[6],
         };
         return <div className="container">
-            <DayPicker
+            {/* <DayPicker
                 firstDayOfWeek={1}
                 selectedDays={selectedDays}
                 showWeekNumbers
@@ -114,13 +122,13 @@ class Calendar extends Component {
                 onDayMouseEnter={this.handleDayEnter}
                 onDayMouseLeave={this.handleDayLeave}
                 onWeekClick={this.handleWeekClick}
-            />
+            /> */}
             <table>
                 <tbody>
                 <tr>
                     {selectedDays.length === 7 &&
                         selectedDays.map((item) => 
-                        (<th className="tbl-date-head">{moment(item).format('DD')}</th>))
+                        (<th className="tbl-date-head" key={item}>{moment(item).format('DD')}</th>))
                     }
                 </tr>
                 <tr>
@@ -135,7 +143,7 @@ class Calendar extends Component {
                 </tr>
 
                 {taskData.map((item) => (
-                    <Task taskData={item} />
+                    <Task taskData={item} key={item.id}/>
                 ))} 
                 </tbody>
             </table>
