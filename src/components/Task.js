6c7b64ecc292a893b53a0ef14fdd05ed3d3044fb
PolinkaLa <miss.lappo@yandex.ru>
2022-10-09 19:37:44 +0300

    refactor Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,83 +1,88 @@
 /* eslint-disable no-loop-func */
-import React, { Component} from 'react';
+import React from 'react';
 import { SHORT_WEEK_DAY, TASK_STATUS } from '../components/Constants';
+import { useSelector,useDispatch } from 'react-redux'
+import { changeStatus, changeText } from './taskListSlicer'
 
-class Task extends Component {
-    constructor(props) {
-        super();
-        this.state = {
-            taskText: props.taskData.taskText,
-        }
-    }
+function Task(props) {
+    const storage = useSelector((state) => state.taskList.taskData);
+    const dispatch = useDispatch();
 
-    changeStatus = event => {
-        const taskID = this.props.taskData.id;
-        const newStatus = event.target.value;
-        const changedDay = event.target.parentElement.className;
-        let storage = JSON.parse(localStorage.plannerApp);
-        const index = storage.findIndex(item => item.id === taskID);
-        storage[index][changedDay]=newStatus;
-        localStorage.plannerApp = JSON.stringify(storage);
+    const updateStatus = (event, newStatus) => {
         const oldStatus = event.target.classList[0];
         event.target.classList.remove(oldStatus);
         event.target.classList.add("status-" + newStatus);
     }
+    const getIndex = (id) => storage.findIndex(item => item.id === id)
 
-    changeTaskText = event => {
-        this.setState({
-            taskText: event.target.value
-        })
-        const taskID = this.props.taskData.id;
-        let storage = JSON.parse(localStorage.plannerApp);
-        const index = storage.findIndex(item => item.id === taskID);
-        storage[index].taskText = event.target.value
-        localStorage.plannerApp = JSON.stringify(storage);
-    }
-
-    render() {
-        const arr = [];
-        let day = "";
-        for(let i = 0; i < 7; i++) {
-            switch (i) {
-                case 0:
-                    day = SHORT_WEEK_DAY[0]
-                    break
-                case 1:
-                    day = SHORT_WEEK_DAY[1]
-                    break
-                case 2:
-                    day = SHORT_WEEK_DAY[2]
-                    break
-                case 3:
-                    day = SHORT_WEEK_DAY[3]
-                    break
-                case 4:
-                    day = SHORT_WEEK_DAY[4]
-                    break
-                case 5:
-                    day = SHORT_WEEK_DAY[5]
-                    break
-                case 6:
-                    day = SHORT_WEEK_DAY[6]
-                    break
-                default:
-                    day = "";
-            }
-            arr.push( <td key={i} className={day}>
-                    <select onChange={this.changeStatus} className={"status-"+this.props.taskData[day]}>
-                        {
-                            TASK_STATUS.map((item) => (
-                                <option key={i+item.value} defaultValue={this.props.taskData[day]===item.value} value={item.value}>{item.label}</option>
-                            ))
-                        }
-                    </select>
-            </td>)
+    const getNewTaskStatus = event => {
+        const newStatus = event.target.value;
+        const changedDay = event.target.parentElement.className;
+        const index = getIndex(props.taskData.id);
+        updateStatus(event, newStatus);
+        return {
+            index,
+            changedDay,
+            newStatus
         }
-        arr.push(<td key='task-text' className="task-text"><input type="text" value={this.state.taskText} onChange={this.changeTaskText}/></td>)
-        return <tr>
-            {arr}
-        </tr>
     }
-}
 
+    const getNewTaskText = event => {
+        const index = getIndex(props.taskData.id);
+        return {
+            index,
+            newText: event.target.value
+        }
+    }
+        
+    const arr = [];
+    let day = "";
+    for(let i = 0; i < 7; i++) {
+        switch (i) {
+            case 0:
+                day = SHORT_WEEK_DAY[0]
+                break
+            case 1:
+                day = SHORT_WEEK_DAY[1]
+                break
+            case 2:
+                day = SHORT_WEEK_DAY[2]
+                break
+            case 3:
+                day = SHORT_WEEK_DAY[3]
+                break
+            case 4:
+                day = SHORT_WEEK_DAY[4]
+                break
+            case 5:
+                day = SHORT_WEEK_DAY[5]
+                break
+            case 6:
+                day = SHORT_WEEK_DAY[6]
+                break
+            default:
+                day = "";
+        }
+        arr.push( <td key={i} className={day}>
+                <select onChange={(e) => {
+                        const newTaskStatus = getNewTaskStatus(e)
+                        dispatch(changeStatus(JSON.stringify(newTaskStatus)))
+                    }} 
+                    className={"status-"+props.taskData[day]}>
+                    {
+                        TASK_STATUS.map((item) => (
+                            <option key={i+item.value} defaultValue={props.taskData[day]===item.value} value={item.value}>{item.label}</option>
+                        ))
+                    }
+                </select>
+        </td>)
+    }
+    arr.push(<td key='task-text' className="task-text"><input type="text" value={props.taskData.taskText} onChange={(e) => {
+        const newText = getNewTaskText(e)
+        dispatch(changeText(JSON.stringify(newText)))}}/></td>)
+    return <tr>
+        {arr}
+    </tr>
+
+}
 export default Task
--- src/components/taskListSlicer.js
+++ src/components/taskListSlicer.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const refreshLocalStorage = (data) => {
+    localStorage.plannerApp = JSON.stringify(data)
+}
+
 export const taskListSlice = createSlice({
     name: 'taskList',
     initialState: {
@@ -13,21 +17,31 @@ export const taskListSlice = createSlice({
         addTask: (state) => {
             state.taskData = [...state.taskData,
                 {
-                    "id": JSON.parse(localStorage.plannerApp).length+1,
-                    "mo": null,
-                    "tu": null,
-                    "we": null,
-                    "th": null,
-                    "fr": null,
-                    "sa": null,
-                    "su": null,
+                    "id": state.taskData.length+1,
+                    "Mo": null,
+                    "Tu": null,
+                    "We": null,
+                    "Th": null,
+                    "Fr": null,
+                    "Sa": null,
+                    "Su": null,
                     "taskText": ""
-                }]
-            localStorage.plannerApp = JSON.stringify(state.taskData)
-        }
+                }];
+                refreshLocalStorage(state.taskData);
+        },
+        changeStatus: (state, data) => {
+            const taskStatus = JSON.parse(data.payload);
+            state.taskData[taskStatus.index][taskStatus.changedDay] = taskStatus.newStatus;
+            refreshLocalStorage(state.taskData);
+        },
+        changeText: (state, data) => {
+            const taskText = JSON.parse(data.payload);
+            state.taskData[taskText.index].taskText = taskText.newText;
+            refreshLocalStorage(state.taskData);
+        } 
     },
 })
 
-export const { reset, addTask } = taskListSlice.actions
+export const { reset, addTask, changeStatus, changeText } = taskListSlice.actions
 
 export default taskListSlice.reducer
